39c91c915b0dc53f3f90fd2c5cd4f088a054f525
KEERTHIPATIVAMSIKRISHNA <152687214+KEERTHIPATIVAMSIKRISHNA@users.noreply.github.com>
2024-07-10 21:17:37 +0530

    Add files via upload

---
+++ studentmarks.js
@@ -0,0 +1,16 @@
+//students average marks//
+let avg = 80;
+let subject1 = 100;
+
+switch(true)
+{
+    case (avg>=75):
+    console.log("grade a");
+    break;
+    case((avg>60)&&(avg<75)):
+    console.log("grade b");
+    break;
+    default:
+        console.log("not eligible")
+
+}
